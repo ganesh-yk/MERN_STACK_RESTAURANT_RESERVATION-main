133c704ef26ec6beef8ccd261f395331cf392d3c
ganesh-yk <ykganeshgani@gmail.com>
2024-04-23 09:04:43 +0530

    home

---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer>
+      <div className="container">
+        <div className="banner">
+          <div className="left">GANESH YK</div>
+          <div className="right">
+            
+          </div>
+        </div>
+        <div className="banner">
+          <div className="left">
+            <p>Developed By GANESH YK</p>
+          </div>
+          <div className="right">
+            <p>All Rights Reserved By GANESH YK.</p>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
